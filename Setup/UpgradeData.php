b10635c67c3edcbde23c7aa33a07eb435f3f3ef3
durga gupta <durgashankar.gupta@javra.com>
2020-04-26 22:47:16 +0545

    add order comment field in DB

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+
+namespace Ace\OrderDeliveryDate\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Quote\Setup\QuoteSetupFactory;
+use Magento\Sales\Setup\SalesSetupFactory;
+
+
+class UpgradeData implements UpgradeDataInterface
+{
+    private $quoteSetupFactory;
+    private $salesSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Quote\Setup\QuoteSetupFactory $quoteSetupFactory
+     */
+    public function __construct(
+        QuoteSetupFactory $quoteSetupFactory,
+        SalesSetupFactory $salesSetupFactory
+    ) {
+        $this->quoteSetupFactory = $quoteSetupFactory;
+
+        $this->salesSetupFactory = $salesSetupFactory;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+        
+    ) {
+        if (version_compare($context->getVersion(), "1.0.2", "<")) {
+
+            $salesSetup = $this->salesSetupFactory->create(['setup' => $setup]);
+            $salesSetup->addAttribute('order', 'delivery_comments',
+                [
+                    'type' => 'date',
+                    'length' => null,
+                    'visible' => false,
+                    'required' => false,
+                    'grid' => true
+                ]
+            );
+    
+            $quoteSetup = $this->quoteSetupFactory->create(['setup' => $setup]);
+            $quoteSetup->addAttribute('quote', 'delivery_comments',
+                [
+                    'type' => 'date',
+                    'length' => null,
+                    'visible' => false,
+                    'required' => false,
+                    'grid' => true
+                ]
+            );
+
+        }
+    }
+}
